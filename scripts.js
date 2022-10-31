d0e3f344776abc1d28e1b1fae806ee3ffdd99889
Kevin McBean <kevin.mcbean@mymona.uwi.edu>
2022-10-30 20:34:55 -0500

    Finished everything

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+
+function subscribe(){
+    document.getElementsByClassName("btn")[1].addEventListener("click", function(){
+        email = document.querySelector("input").value;
+        msg = document.querySelector(".message")
+        event.preventDefault()
+        if(email.includes("@") && email.includes(".")){
+            msg.textContent = "Thank you! Your email address " + email + " has been added to our mailing list!"
+        }
+        else{
+            msg.textContent = "Please enter a valid email address."
+        }
+    })
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+    subscribe();
+})
